e2da5e02a98943f312092a84eeee5c83031566d5
CH <jeongcw95@naver.com>
2018-04-16 19:58:53 +0900

    initial commit

---
+++ Application/src/main/java/com/example/android/wearable/recipeassistant/MainPage.java
@@ -0,0 +1,29 @@
+package com.example.android.wearable.recipeassistant;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+public class MainPage extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainpage);
+
+
+
+        }
+
+    public void StartButtonClicked(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void FavoriteButtonClicked(View view) {
+        Intent intent = new Intent(this, Favorite.class);
+        startActivity(intent);
+    }
+}
+
---
+++ Application/src/main/java/com/example/android/wearable/recipeassistant/Splash.java
@@ -0,0 +1,23 @@
+package com.example.android.wearable.recipeassistant;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            Thread.sleep(3000);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this,LoginActivity.class));
+        finish();
+        /*Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+        */
+    }
+}
