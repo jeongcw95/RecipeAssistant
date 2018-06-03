ecb6aae27244ceeef5fadc30b0224fb761a384d9
Jaeyeol-Seo <sjy940307@gmail.com>
2018-06-04 03:27:16 +0900

    cccccccccc

--- Application/src/main/java/com/example/android/wearable/recipeassistant/Splash.java
+++ Application/src/main/java/com/example/android/wearable/recipeassistant/Splash.java
@@ -3,36 +3,45 @@ package com.example.android.wearable.recipeassistant;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 public class Splash extends Activity {
     databaseHelper dh;
     static User LoginUser;
     static boolean AutoLoginActive ;
     public static int AutoLoginChecked;
-
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AutoLoginActive = false;
-        LoginUser = new User();
-        dh = new databaseHelper(getApplicationContext());
-        AutoLoginChecked = 0;
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        if (!(dh.CheckAutoLogin(1) == null)){
-            AutoLoginActive = true;
-            LoginUser = dh.CheckAutoLogin(1);
-            AutoLoginChecked = 1;
-            startActivity(new Intent(this, MainPage.class));
-            finish();
-        }
-        else {
-            AutoLoginActive = false;
-            startActivity(new Intent(this, LoginActivity.class));
-            finish();
-        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                AutoLoginActive = false;
+                LoginUser = new User();
+                dh = new databaseHelper(getApplicationContext());
+                AutoLoginChecked = 0;
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (!(dh.CheckAutoLogin(1) == null)){
+                    AutoLoginActive = true;
+                    LoginUser = dh.CheckAutoLogin(1);
+                    AutoLoginChecked = 1;
+                    startActivity(new Intent(Splash.this, MainPage.class));
+                    finish();
+                }
+                else {
+                    AutoLoginActive = false;
+                    startActivity(new Intent(Splash.this, LoginActivity.class));
+                    finish();
+                }
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
     }
 }
